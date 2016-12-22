1ad5230639458269559519c8515ab9df7d061aae
alviyanrahmad <alviyanrahmad@gmail.com>
2016-12-22 17:48:01 +0700

    Add files via upload

---
+++ AplikasiChattingRMI/Chat_Server/src/chat_server/Chat_Server.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat_server;
+
+/**
+ *
+ * @author Prakoso
+ */
+public class Chat_Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
